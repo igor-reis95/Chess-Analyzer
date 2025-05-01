1e14ebb518036c8799ce0ad1e10b56ace729af73
Igor Reis <igor_rubbo@hotmail.com>
2025-05-01 14:35:51 -0300

    Adicionada função que executará uma pipeline de dados

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 # main.py
 from src.api import get_games
-from src.data_processing import save_games_to_json
+from src.data_processing import flatten_game_data, save_df_to_csv
 from src import app
+
+def run_pipeline(username: str, max_games: int, perf_type: str):
+    games_list = get_games(username, max_games, perf_type)
+    df = flatten_game_data(games_list)
+    save_df_to_csv(df, username)
+    return df
 
 if __name__ == "__main__":
     app.run(debug=True)
