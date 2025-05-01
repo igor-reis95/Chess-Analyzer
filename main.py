7aa7111772cd2cfe9b43a26640c239d80470b335
Igor Reis <igor_rubbo@hotmail.com>
2025-05-01 10:12:58 -0300

    Dados de teste retirados da main

--- main.py
+++ main.py
@@ -3,16 +3,5 @@ from src.api import get_games
 from src.data_processing import save_games_to_json
 from src import app
 
-def main():
-    username = "IgorSReis"  # Exemplo de nome de usuário
-    max_games = 50
-    perf_type = "Bullet"
-    
-    # Coletar os jogos da API
-    games_list = get_games(username, max_games, perf_type)
-    
-    # Salvar os jogos em um arquivo JSON
-    save_games_to_json(games_list, username)
-
 if __name__ == "__main__":
     app.run(debug=True)
