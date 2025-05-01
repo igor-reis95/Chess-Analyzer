4f679988018bf9b15d7beee4e8f3457f5588bc1f
Igor Reis <igor_rubbo@hotmail.com>
2025-05-01 10:12:08 -0300

    Utilizar dados enviados pelo usuário para puxar dados do Lichess

--- src/routes.py
+++ src/routes.py
@@ -3,9 +3,19 @@ from flask import request, render_template
 from src.api import get_games
 from src.data_processing import save_games_to_json
 
-@app.route("/", methods=["GET"])
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("index.html")
+    if request.method == "POST":
+        username = request.form["username"]
+        max_games = int(request.form.get("max_games", 50))
+        perf_type = request.form.get("perf_type", "blitz")
+        color = request.form.get("color", None)
+
+        games = get_games(username, max_games, perf_type, color)
+
+        return render_template("result.html", username=username, count=len(games))
+
+    return render_template("form.html")
 
 @app.route("/buscar", methods=["POST"])
 def buscar():
