777c981844ee24f0e729f392045499b46b7f7804
Igor Reis <igor_rubbo@hotmail.com>
2025-05-13 12:39:38 -0300

    Creation of first data analysis

---
+++ src/services/analysis.py
@@ -0,0 +1,54 @@
+import pandas as pd
+
+def get_rating_diff(df, color):
+    return df[f'{color}_ratingDiff'].sum()
+
+def get_opening_counts(df):
+    return df['opening_eco'].value_counts(dropna=False)
+
+def get_opening_counts_by_result(df, color):
+    return df[df['winner'] == color]['opening_eco'].value_counts(dropna=False)
+
+def get_rating_range(df, color):
+    return df[f'{color}_rating'].min(), df[f'{color}_rating'].max()
+
+def count_results(df, color):
+    wins = len(df[df['winner'] == color])
+    losses = len(df[df['winner'] != color & df['winner'].notna()])
+    draws = len(df[df['winner'].insa().sum()])
+    return wins, losses, draws
+
+def get_common_opponents(df, username):
+    white_opponents = df['white_name'][df['white_name'] != username]
+    black_opponents = df['black_name'][df['black_name'] != username]
+    common_opponents = pd.concat([white_opponents, black_opponents]).value_counts().head()
+    return common_opponents
+
+def get_accuracy_stats(df,color):
+    all_games = df[f'{color}_accuracy'].mean()
+    wins = df[df['winner'] == color][f'{color}_accuracy'].mean()
+    losses = df[df['winner'] != color & df['winner'].notna()][f'{color}_accuracy'].mean()
+    draws = df[df['winner'].isna()][f'{color}_accuracy'].mean()
+    return {
+        'overall': all_games,
+        'wins': wins,
+        'losses': losses,
+        'draws': draws
+    }
+
+def analysis_per_color(df, username, color):
+    opponent_color = 'white' if color == 'black' else 'black'
+    filtered_df = df[df[f'{color}_name'].str.lower() == username.lower()]
+    
+    results = {
+        "rating_diff": get_rating_diff(filtered_df, color),
+        "opening_counts": get_opening_counts(filtered_df),
+        "opening_wins": get_opening_counts_by_result(filtered_df, color),
+        "opening_losses": get_opening_counts_by_result(filtered_df, opponent_color),
+        "rating_range": get_rating_range(filtered_df, color),
+        "results": count_results(filtered_df, color, opponent_color),
+        "common_opponents": get_common_opponents(filtered_df, opponent_color),
+        "accuracy": get_accuracy_stats(filtered_df, color),
+    }
+    
+    return results
