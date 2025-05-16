e41252a3d917c95c3adfa71c16a0f22100e462f7
Igor Reis <igor_rubbo@hotmail.com>
2025-05-16 12:15:17 -0300

    Pylint stuff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+# pylint: disable=missing-module-docstring, missing-class-docstring, missing-function-docstring
 from src.webapp import app
 
 if __name__ == "__main__":
     app.run(debug=True)
-    
