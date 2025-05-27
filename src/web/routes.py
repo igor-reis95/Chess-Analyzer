1478c2de1f430fb05edb15d1668d22815f01f866
Igor Reis <igor_rubbo@hotmail.com>
2025-05-27 12:15:57 -0300

    Added overall analysis, aside from just colored and added a common opponents table

--- src/web/routes.py
+++ src/web/routes.py
@@ -1,7 +1,7 @@
 # pylint: disable=missing-module-docstring, missing-class-docstring, missing-function-docstring
 import traceback
 from flask import render_template, request
-from src.services.analysis import analysis_per_color
+from src.services.analysis import basic_analysis
 from src.services.data_viz import status_distribution
 from src.services.game_processor import GameProcessor
 from ..webapp import app
@@ -23,10 +23,17 @@ def index():
             processor.run_all()
             df = processor.get_dataframe()
             game_records = df.to_dict(orient='records')
+            game_records_for_table = df.head(30).to_dict(orient='records')
 
             # Run the data analysis
-            analysis_for_white = analysis_per_color(df, 'white')
-            analysis_for_black = analysis_per_color(df, 'black')
+            overall_analysis = basic_analysis(df)
+            analysis_for_white = basic_analysis(df, 'white')
+            analysis_for_black = basic_analysis(df, 'black')
+            common_opponents_html = overall_analysis['common_opponents'].to_frame().to_html(
+                classes="table table-striped",  # Bootstrap classes (optional)
+                header=True, 
+                index=False
+            )
 
             # Generate graphs
             status_distribution_graph = status_distribution(df)
@@ -35,9 +42,11 @@ def index():
             return render_template("result.html",
                                    username=username,
                                    count=len(game_records),
-                                   games = game_records,
+                                   games_table = game_records_for_table,
+                                   overall_analysis = overall_analysis,
                                    analysis_for_white = analysis_for_white,
                                    analysis_for_black = analysis_for_black,
+                                   common_opponents = common_opponents_html,
                                    status_distribution_graph=status_distribution_graph)
         except Exception as e:
             # Handle any error that happens during fetching the games
