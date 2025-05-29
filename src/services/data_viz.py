12d8ef1da2747e3a7615cdfe72a1468127e2a673
Igor Reis <igor_rubbo@hotmail.com>
2025-05-29 16:53:11 -0300

    Changed colors of graphs

--- src/services/data_viz.py
+++ src/services/data_viz.py
@@ -19,10 +19,10 @@ def winrate_bar_graph(data):
     fig, ax = plt.subplots()
 
     # Stack the bars
-    p1 = ax.bar(x, wins, bar_width, label='win', color='#4CAF50')
-    p2 = ax.bar(x, draws, bar_width, bottom=wins, label='draw', color='#FFC107')
+    p1 = ax.bar(x, wins, bar_width, label='win', color='#92b76f')
+    p2 = ax.bar(x, draws, bar_width, bottom=wins, label='draw', color='#d59c4d')
     p3 = ax.bar(x, losses, bar_width,
-                bottom=[i + j for i, j in zip(wins, draws)], label='loss', color='#F44336')
+                bottom=[i + j for i, j in zip(wins, draws)], label='loss', color='#db6f72')
 
     # Labels
     ax.set_ylabel('Percentage')
@@ -52,24 +52,34 @@ def winrate_bar_graph(data):
     return img_base64
 
 def plot_game_status_distribution(df):
-    # Count the occurrences of each status
     status_counts = df['status'].value_counts()
 
-    # Create the pie chart
+    custom_colors = []
+    for status in status_counts.index:
+        if status == 'resign':
+            custom_colors.append('#93b674')
+        elif status == 'mate':
+            custom_colors.append('#da6f73')
+        elif status == 'draw':
+            custom_colors.append('#d49b54')
+        elif status == 'outoftime':
+            custom_colors.append('#3288d1')
+        else:
+            custom_colors.append('#6c757d')
+
     fig, ax = plt.subplots()
     wedges, texts, autotexts = ax.pie(
         status_counts,
         labels=status_counts.index,
         autopct='%1.1f%%',
         startangle=90,
-        wedgeprops={'width': 0.4}
+        wedgeprops={'width': 0.4},
+        colors=custom_colors
     )
 
-    # Add a white circle in the center to make it a donut chart
     centre_circle = plt.Circle((0, 0), 0.70, fc='white')
     fig.gca().add_artist(centre_circle)
 
-    # Set the aspect ratio to be equal for the pie chart to be circular
     ax.axis('equal')
     plt.title("Game Status Distribution")
     ax.legend(
@@ -80,17 +90,11 @@ def plot_game_status_distribution(df):
         bbox_to_anchor=(1, 0.5)
     )
 
-    # Adjust layout
     plt.tight_layout()
 
-    # Save the plot to a BytesIO object
     img_stream = io.BytesIO()
     plt.savefig(img_stream, format='png')
     img_stream.seek(0)
-
-    # Encode the image in base64
     img_base64 = base64.b64encode(img_stream.read()).decode('utf-8')
-
-    # Return the base64 string
     plt.close()
     return img_base64
