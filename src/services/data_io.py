245f20ce8ce64f2bc83a11f45900da9c936cd83b
Igor Reis <igor_rubbo@hotmail.com>
2025-06-11 19:00:55 -0300

    Added module to save data to a postgres database

--- src/services/data_io.py
+++ src/services/data_io.py
@@ -9,6 +9,13 @@ Files are stored under configurable folders (default: data/raw and data/processe
 
 import os
 import json
+import pandas as pd
+from sqlalchemy import create_engine
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("local_database_url")
 
 def save_games_to_json(games_list, username, folder="data/raw"):
     """Save the raw games list as a JSON file."""
@@ -24,3 +31,23 @@ def save_df_to_csv(df, username, folder="data/processed"):
     filepath = os.path.join(folder, f"{username}_games.csv")
     df.to_csv(filepath, index=False)
     print(f"Saved processed games to {filepath}")
+
+def save_processed_game_data(df, table = 'games_processed_data'):
+    """Save processed data from games into a postgreSQL database managed by Render"""
+    # Create SQLAlchemy engine
+    engine = create_engine(DATABASE_URL)
+
+    # Get existing IDs from database
+    existing_ids = pd.read_sql("SELECT id FROM games_processed_data", engine)['id'].tolist()
+
+    # Filter DataFrame to exclude existing IDs
+    df_to_insert = df[~df['id'].isin(existing_ids)]
+
+    # Write to SQL table in the public schema
+    df_to_insert.to_sql(
+        name=table,
+        con=engine,
+        schema='public',     # explicitly specify schema
+        if_exists='replace', # options: 'fail', 'replace', 'append'
+        index=False
+    )
