f1b26d9c1a7681be3715301383e6aa2fe14fcd41
Igor Reis <igor_rubbo@hotmail.com>
2025-07-31 10:04:00 -0300

    Removed code to save data do JSON or to CSV as it was used mainly for debugging while developing

--- src/services/data_io.py
+++ src/services/data_io.py
@@ -15,21 +15,6 @@ from psycopg2.extras import execute_values
 
 DATABASE_URL = os.getenv("database_url")
 
-def save_games_to_json(games_list, username, folder="data/raw"):
-    """Save the raw games list as a JSON file."""
-    os.makedirs(folder, exist_ok=True)
-    filepath = os.path.join(folder, f"{username}_games.json")
-    with open(filepath, "w", encoding="utf-8") as f:
-        json.dump(games_list, f, indent=2, ensure_ascii=False)
-    print(f"Saved games to {filepath}")
-
-def save_df_to_csv(df, username, folder="data/processed"):
-    """Save the processed games DataFrame as a CSV file."""
-    os.makedirs(folder, exist_ok=True)
-    filepath = os.path.join(folder, f"{username}_games.csv")
-    df.to_csv(filepath, index=False)
-    print(f"Saved processed games to {filepath}")
-
 def save_processed_game_data(conn, df, table='games_processed_data'):
     """Append all rows in df to the table using fast psycopg2 bulk insert"""
 
